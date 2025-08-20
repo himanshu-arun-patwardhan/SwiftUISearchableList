79bb98af28bb4c35cc3a0e062e125b940f8b7fd1
himanshu-arun-patwardhan <himanshu.arun.patwardhan@gmail.com>
2025-08-20 14:24:38 +0530

    [github web commit] - [clean code writing] - Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,20 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUISearchableList",
-    platforms: [
-        .iOS(.v17)
-    ],
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftUISearchableList",
-            targets: ["SwiftUISearchableList"]),
+            targets: ["SwiftUISearchableList"]
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftUISearchableList"),
-
+            name: "SwiftUISearchableList")
     ]
 )
